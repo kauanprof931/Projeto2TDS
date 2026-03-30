8d082d1e3e9025f5403ae9ab308c71d1df96457b
Kauan Sousa <k.sobrinho@escola.pr.gov.br>
2026-03-30 11:54:46 -0300

    Operadores Aritméticos

---
+++ manualUsuario/operadores.js
@@ -0,0 +1,18 @@
+// Operadores Aritméticos
+
+let valor1 = 10;
+let valor2 = 3;
+
+console.log("Operadores Aritméticos");
+console.log("Adição: 10 + 3 = ", valor1 + valor2);
+console.log("Subtração: 10 - 3 = ", valor1 - valor2);
+console.log("Multiplicação: 10 * 3 = ", valor1 * valor2);
+console.log("Divisão: 10 / 3 = ", valor1 / valor2);
+
+// Operadores de Comparação
+
+console.log("Operadores Comparatios");
+console.log("Igualdade: 10 == 3 = ", valor1 == valor2);
+console.log("Desigualdade: 10 != 3 = ", valor1 != valor2);
+console.log("Maior ou igual: 10 >= 3 = ", valor1 >= valor2);
+console.log("Menor ou igual: 10 <= 3 = ", valor1 <= valor2);
