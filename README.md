4ba8887c67398be96c5ebcc5f2af2623d1de06d1
Kauan Sousa <k.sobrinho@escola.pr.gov.br>
2026-04-01 11:09:39 -0300

    Operadores Comparativos

--- README.md
+++ README.md
@@ -17,3 +17,16 @@ Para realizar o envio
 Essas etapas devem ser seguidas em ordem, também não podemos
 ignorar nenhum comando, pois isso pode fazer nosso commit não
 ser enviado.
+
+
+Atribui a função diretamente a variável
+const somar = function(a, b) {
+    return a + b;
+};
+
+const saudacao = function(nome) {
+    return `Olá, ${nome}!`;
+};
+
+Método moderno
+const saudacao = (nome) => `Olá, ${nome}!`;
--- manualUsuario/operadores.js
+++ manualUsuario/operadores.js
@@ -1,7 +1,8 @@
 // Operadores Aritméticos
+// São utilizados para realizar cálculos matemáticos
 
 let valor1 = 10;
-let valor2 = 3;
+let valor2 = 10;
 
 console.log("Operadores Aritméticos");
 console.log("Adição: 10 + 3 = ", valor1 + valor2);
@@ -9,10 +10,12 @@ console.log("Subtração: 10 - 3 = ", valor1 - valor2);
 console.log("Multiplicação: 10 * 3 = ", valor1 * valor2);
 console.log("Divisão: 10 / 3 = ", valor1 / valor2);
 
-// Operadores de Comparação
-
-console.log("Operadores Comparatios");
+// Operadores Comparativos
+// São utilzados para realizar uma comparação e obter um valor lógico a partir dela
+console.log("Operadores Comparativos");
 console.log("Igualdade: 10 == 3 = ", valor1 == valor2);
 console.log("Desigualdade: 10 != 3 = ", valor1 != valor2);
 console.log("Maior ou igual: 10 >= 3 = ", valor1 >= valor2);
 console.log("Menor ou igual: 10 <= 3 = ", valor1 <= valor2);
+console.log("Maior: 10 > 3 = ", valor1 > valor2);
+console.log("Menor: 10 < 3 = ", valor1 < valor2);
